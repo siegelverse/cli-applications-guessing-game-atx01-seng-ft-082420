32305f5c96eec7a0f86ca48b3366d57d1d310351
Marcus Siegel <msiegel37@gmail.com>
2020-08-22 19:32:05 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -3,8 +3,9 @@ def run_guessing_game
   puts "Guess a number from 1-6"
   user_input = gets.chomp
   rand_num = rand(1..6)
-  while user_input != "exit" do 
-   if user_input == rand_num
+  while user_input == "exit" do 
+    puts "Goodbye!"
+   elsif user_input == rand_num
    puts "You guessed the correct number!"
    elsif user_input != rand_num
    puts "Sorry! The computer guessed #{rand_num}."
